5176ed7c6a330e0dadfabebf4bef56fd3a2fffc1
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-03-18 14:40:35 +0100

    Created rest API

---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from rest_framework.routers import SimpleRouter
+
+from .views import PostViewset, UserViewset
+
+
+router = SimpleRouter()
+router.register('users', UserViewset, basename='users')
+router.register('', PostViewset, basename="posts")
+
+urlpatterns = router.urls
---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
+
+
+class PostViewset(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
